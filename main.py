d4cf489058f109b91831ebbfcfeef11c1f9f05d3
eng1neee <exiver1593@mail.ru>
2023-11-02 18:25:15 +0200

    database update, add login, logout, auth

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from datetime import datetime
 from enum import Enum
 from typing import List, Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi_users import fastapi_users, FastAPIUsers
 from pydantic import BaseModel, Field
+
+from auth.auth import auth_backend
+from auth.manager import get_user_manager
+from auth.schemas import UserRead, UserCreate
 
 app = FastAPI(
     title="Trading App"
@@ -80,3 +85,30 @@ class Trade(BaseModel):
 def add_trades(trades: List[Trade]):
     fake_trades.extend(trades)
     return {"status": 200, "data": fake_trades}
+
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+current_user = fastapi_users.current_user()
+
+
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
--- models/models.py
+++ models/models.py
@@ -1,24 +1,27 @@
 from datetime import datetime
 
-from sqlalchemy import MetaData, Integer, String, TIMESTAMP, ForeignKey, Table, Column, JSON
+from sqlalchemy import MetaData, Integer, String, TIMESTAMP, ForeignKey, Table, Column, JSON, Boolean
 
 metadata = MetaData()
 
-roles = Table(
-    "roles",
+role = Table(
+    "role",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("Name", String, nullable=False),
     Column("Permissions", JSON),
 )
 
-users = Table(
-    "users",
+user = Table(
+    "user",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("email", String, nullable=False),
     Column("username", String, nullable=False),
-    Column("password", String, nullable=False),
-    Column("registered_at", TIMESTAMP, default=datetime.utcnow()),
-    Column("role_id", Integer, ForeignKey("roles.id")),
+    Column("registered_at", TIMESTAMP, default=datetime.utcnow),
+    Column("role_id", Integer, ForeignKey(role.c.id)),
+    Column("hashed_password", String, nullable=False),
+    Column("is_active", Boolean, default=True, nullable=False),
+    Column("is_superuser", Boolean, default=False, nullable=False),
+    Column("is_verified", Boolean, default=False, nullable=False),
 )
